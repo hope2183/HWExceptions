2d88e8744c26018e79cc5d7b2c960280780c4c9e
hope2183@mail.ru <hope2183@mail.ru>
2022-01-25 08:53:16 +1100

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/hwexceptions/HwExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hwexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwExceptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
